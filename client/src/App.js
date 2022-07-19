88df0d569fe68a2312ac99436704f2288ac1ef39
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-07-19 15:56:17 -0600

    React setup

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Logo } from './Logo';
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Logo h="40vmin" pointerEvents="none" />
+            <Text>
+              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+            </Text>
+            <Link
+              color="teal.500"
+              href="https://chakra-ui.com"
+              fontSize="2xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn Chakra
+            </Link>
+            <div className="App">
+              <h1>Page Count: {count}</h1>
+            </div>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
