42b704708dda8df44a8f5ee6cbfdd53d442d29f8
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-07-24 14:57:47 -0600

    257

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,8 +1,13 @@
 class Member < ApplicationRecord
-
   has_secure_password
 
-  # has_many :recipes, dependent: :destroy
+  has_many :team_assignments, dependent: :destroy
+  has_many :teams, through: :team_assignments
+  has_many :owned_teams, class_name: "Team"
 
+  has_many :task_assignments, dependent: :destroy
+  has_many :tasks, through: :task_assignments
+
+  
   validates :username, presence: true, uniqueness: true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,9 @@
 class Team < ApplicationRecord
   belongs_to :member
+
+  has_many :team_assignments, dependent: :destroy
+  has_many :members, through: :team_assignments
+
+  validates :team_name, presence: true, uniqueness: true
+  
 end
